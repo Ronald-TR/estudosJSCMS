96c40aac4b4c98b1835fadd77b1d1c4876c31838
Ronald-TR <ronald-farias@outlook.com>
2018-02-24 01:45:29 -0300

    corrigindo ckeditor

--- djangocmsrest/djangocmsrest/static/core/js/app.js
+++ djangocmsrest/djangocmsrest/static/core/js/app.js
@@ -1,4 +1,4 @@
-try {
+/*try {
     document.getElementById('title').innerHTML = localStorage['title'] || 'Just Write';
     document.getElementById('content').innerHTML = localStorage['content'] || 'This text is auto saved';
 } catch (error) {
@@ -9,7 +9,7 @@ setInterval(function(){
     localStorage['title'] = document.getElementById('title').innerHTML;
     localStorage['content'] = document.getElementById('content').innerHTML;
 }, 1000);
-
+*/
 $("#btn_text").click(function(){
     var token = $("#box_text").children()[1-1]; // fiz isso pois a tecla zero esta quebrada
     var tokenname = token.name;
