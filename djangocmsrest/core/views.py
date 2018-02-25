f8b5899abe10dbf8aeaee00fcac3ec4192ed02d8
Ronald-TR <ronald-farias@outlook.com>
2018-02-25 11:32:40 -0300

    inserindo bloqueio de AnonymousUser

--- djangocmsrest/core/views.py
+++ djangocmsrest/core/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout, get_user
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import AnonymousUser
 from core import libutils
 from core.models import *
 
 import json
+
+
+#def cadaster(request):
 
 
 def writerlogin(request):
@@ -20,13 +24,25 @@ def writerlogin(request):
         return redirect('/errorlogin')
 
 def errorlogin(request):
-    return render(request, 'error_login.html')
+    error = {
+        'errormsg': 'Erro ao autenticar',
+        'errordetail': 'Por favor... retorne ao inicio e tente novamente'
+    }
+    return render(request, 'error_page.html', error)
 
 
 def loginpage(request):
     return render(request, 'login_page.html')
 
+
 def postwrite(request):
+    user  = get_user(request)
+    error = {
+        'errormsg': 'Desculpe, anônimo',
+        'errordetail': 'Usuarios anonimos "ainda"  não podem escrever, logue-se ou cadastre-se'
+    }
+    if type(user) is AnonymousUser:
+        return render(request, 'error_page.html', error)
     return render(request, 'post_write_page.html')
 
 def postlist(request):
